093db0f437663a83498be98c149ae989ad5518d0
Javier de la Rosa <versae@gmail.com>
2013-09-29 20:56:39 -0400

    Add production settings

--- hatersgonnagit/settings.py
+++ hatersgonnagit/settings.py
@@ -1,7 +1,7 @@
 # Django settings for hatersgonnagit project.
 from os import environ, path
 
-DEBUG = environ.get('GITHUB_KEY', True)
+DEBUG = environ.get('DEBUG', True)
 TEMPLATE_DEBUG = DEBUG
 
 PROJECT_NAME = u"Haters Gonna Git"
@@ -27,7 +27,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
